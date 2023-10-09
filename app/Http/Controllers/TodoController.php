1b02ad08c843f73b3885f38bdc86f42368169579
Ben Kershaw <n00221365@iadt.ie>
2023-10-09 11:56:02 +0100

    Created Views and Seeders

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::orderBy('created_at', 'desc')->paginate(8);
+        return view('todos.index', [
+            'todos' => $todos
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+The todos view is working :D
+
+<h2> Todos</h2>
+
+<table>
+<thead> 
+    <tr>
+    <th>Title</th>
+    <th>Snippet</th>
+    <th>Created</th>
+    <th></th>
+    </tr>
+</thead>
+
+<tbody>
+
+    @forelse($todos as $todo)
+
+    <tr>
+        <td>{{ $todo->title}}</td>
+        <td>{{ $todo->body}}</td>
+        <td>{{ $todo->created_at}}</td>
+        <td><a href="#" >Read More</a></td>
+    </tr>
+        @empty 
+        <h4>No Todos Found!</h4>
+        
+    @endforelse
+
+</tbody>
+</table>
+@endsection
